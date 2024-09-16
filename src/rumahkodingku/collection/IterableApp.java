9270ec2026235522eac8be12aea398e726e27506
thomasalbertoDev <thomasalberto456@gmail.com>
2024-09-16 10:37:21 +0700

    Belajar Java Collection | Iterable

--- src/rumahkodingku/collection/IterableApp.java
+++ src/rumahkodingku/collection/IterableApp.java
@@ -2,12 +2,13 @@ package rumahkodingku.collection;
 
 import java.util.List;
 
-//Iterable merupakan
+// Iterable adalah antarmuka (interface) di Java yang menyediakan kemampuan untuk mengulangi (iterasi) elemen di dalam suatu collection.
+// Iterator adalah antarmuka yang menyediakan cara untuk mengakses elemen-elemen dari koleksi satu per satu, tanpa memerlukan pengenalan detail internal dari koleksi tersebut.
 public class IterableApp {
     public static void main(String[] args) {
-        Iterable<String> names = List.of("Thomas " , "Alberto");
+        Iterable<String> names = List.of("Thomas ", "Alberto");
 
-        for (var name : names){
+        for (var name : names) {
             System.out.println(name);
         }
     }
